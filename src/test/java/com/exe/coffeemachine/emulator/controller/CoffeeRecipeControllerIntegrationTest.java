c5cd74c4138117f38c79fd1523b0524fae1f55fb
Bondarev Oleg <bondarev@mail.ru>
2024-08-02 13:48:52 +0300

    update tests. Added rollback

--- src/test/java/com/exe/coffeemachine/emulator/controller/CoffeeRecipeControllerIntegrationTest.java
+++ src/test/java/com/exe/coffeemachine/emulator/controller/CoffeeRecipeControllerIntegrationTest.java
@@ -11,6 +11,8 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -19,6 +21,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  * @author user
  * @year 2024
  */
+@Transactional
+@Rollback
 @SpringBootTest
 public class CoffeeRecipeControllerIntegrationTest {
 
